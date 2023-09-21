b4c81219264cb3936b14c79456852a1dd27fc0a9
Noemi Cunha <noemicunha@live.com>
2023-09-20 23:27:28 -0300

    entrega resolução do exercicio para casa

---
+++ exercicios/para-casa/entregas/noemi-cunha/resolucao.js
@@ -0,0 +1,50 @@
+class Conta {
+	constructor(nome, saldo, limite, limiteativo) {
+		this.nome = nome;
+		this.saldo = saldo;
+		this.limite = limite;
+		this.limiteativo = limiteativo;
+	}
+
+	consultaSaldo() {
+		return this.saldo;
+	}
+
+	consultaLimite() {
+		if (this.limiteativo) {
+			return this.limite;
+		}
+	}
+
+	deposito(valor) {
+		this.saldo = this.saldo + valor;
+	}
+
+	saque(valor) {
+		if (this.limiteativo) {
+			if (valor <= this.saldo + this.limite) {
+				this.saldo = this.saldo - valor;
+			} else {
+				return "Saldo indisponível";
+			}
+		} else {
+			return "Limite não ativo";
+		}
+	}
+
+	ajusteDoLimite(valor) {
+		if (this.limiteativo) {
+			this.limite = valor;
+		}
+	}
+
+	desativaLimite() {
+		this.limiteativo = !this.limiteativo;
+	}
+
+	ativaLimite() {
+		return this.limiteativo;
+	}
+}
+
+module.exports = Conta;
---
+++ exercicios/para-casa/entregas/noemi-cunha/resolucao.test.js
@@ -0,0 +1,49 @@
+const Conta = require("./resolucao");
+
+describe("Conta Bancária", () => {
+	beforeEach(() => {
+		conta = new Conta("André", 1500, 500, true);
+		conta2 = new Conta("Maria", 1000, 500, false);
+	});
+
+	test("Confere a funcionalidade de consulta ao saldo", () => {
+		expect(conta.consultaSaldo()).toBe(1500);
+	});
+	test("Testa a funcionalidade de depósito na conta", () => {
+		conta.deposito(1000);
+		expect(conta.consultaSaldo()).toBe(2500);
+	});
+	test("Testa a funcionalidade de saque na conta", () => {
+		conta.saque(1000);
+		expect(conta.consultaSaldo()).toBe(500);
+	});
+	test("Testa a funcionalidade de saque na conta além do saldo", () => {
+		conta.saque(1700);
+		expect(conta.consultaSaldo()).toBe(-200);
+	});
+	test("Testa a funcionalidade de saque na conta além do limite", () => {
+		const resultado = conta.saque(10000);
+		expect(resultado).toBe("Saldo indisponível");
+		expect(conta.consultaSaldo()).toBe(1500);
+	});
+	test("Testa a funcionalidade de saque na conta sem limite ativo", () => {
+		const resultado = conta2.saque(200);
+		expect(resultado).toBe("Limite não ativo");
+		expect(conta2.consultaSaldo()).toBe(1000);
+	});
+	test("Testa a funcionalidade de alteração de limite para mais", () => {
+		conta.ajusteDoLimite(1000);
+		expect(conta.consultaLimite()).toBe(1000);
+	});
+	test("Testa a funcionalidade de alteração de limite para menos", () => {
+		conta.ajusteDoLimite(200);
+		expect(conta.consultaLimite()).toBe(200);
+	});
+	test("Testa a funcionalidade de desativar e reativar limite", () => {
+		conta.desativaLimite();
+		expect(conta.ativaLimite()).toBe(false);
+
+		conta2.desativaLimite();
+		expect(conta2.ativaLimite()).toBe(true);
+	});
+});
